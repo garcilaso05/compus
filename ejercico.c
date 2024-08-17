8800705ba267151150e93f4db7653bf4ef9cbc9a
garcilaso05 <rogergarciadoncel@gmail.com>
2024-08-18 00:51:02 +0200

    Probando IA

--- ejercico.c
+++ ejercico.c
@@ -1,1 +1,19 @@
-//No tengo ni idea de que estoy haciendo :((
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main(){
+
+    printf("Hello World!\n");
+
+    char *str = "Hello World!\n";
+    printf("%s", str);
+    free(str);
+    str = NULL;
+    printf("%s", str); // Output: NULL
+    printf("%s", str); // Output: NULL
+    printf("%s", str);  // Output: NULL
+
+    char *str2 = malloc(sizeof(char) * (strlen("Hello World!\n") + 1));
+}
